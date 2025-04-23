546d7becb95313234295f6f781bb6f012368bf43
meqhh <fazrimyazid@gmail.com>
2025-04-23 11:20:46 +0700

    [ADD] Module Content

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Model view Relation show',
+    'version': '1.0',
+    'summary': 'Showing Relation what related model for Many2one, One2many, and Many2many',
+    'sequence': 2,
+    'description': """
+    This module make field with type Many2one, One2many, and Many2many on ir.model form view shows the related model.
+    """,
+    'category': 'other',
+    'website': 'https://www.github.com/meqhh',
+    'depends': [
+    ],
+    'license': 'LGPL-3',
+    'data': [
+        # views
+        "views/ir_model_view.xml",
+    ],
+    'assets': {
+        'point_of_sale._assets_pos': [
+        ],
+    },
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
